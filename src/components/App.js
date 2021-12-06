336f59899793342fa946bdb459db32a410e5279c
Jessen Mercado <jessen.mercado@metafinanciera.com>
2021-12-06 12:11:47 -0600

    Metiendole async para esperar respuestas de json placeholder con los usuarios

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,27 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 class App extends Component {
   constructor() {
     super()
     this.state = {
+      usuarios: [],
+    }
+  }
+
+  async componentDidMount() {
+    const respuesta = await axios.get(
+      'https://jsonplaceholder.typicode.com/users'
+    )
+    console.log(`Respuesta: ${respuesta}`)
+    this.setState({
       usuarios: [
         { nombre: 'jessen', correo: 'Jessen@gmail.com', enlace: 'Jessen.com' },
         { nombre: 'beth', correo: 'beth@gmail.com', enlace: 'beth.com' },
       ],
-    }
+    })
   }
+
   ponerFilas = () =>
     this.state.usuarios.map((usuario) => (
       <tr>
@@ -20,6 +32,7 @@ class App extends Component {
     ))
 
   render() {
+    console.log(this.state.usuarios)
     return (
       <div className='margen'>
         <table className='tabla'>
